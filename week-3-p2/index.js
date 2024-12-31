e5207e4508746204906ab17ba0de24fb9b0297c7
fira-g <firaolgula@gmail.com>
2024-12-31 09:32:26 +0300

    added project 2

---
+++ week-3-p2/index.js
@@ -0,0 +1,15 @@
+const lod = require('lodash')
+
+function findMinMax(arr){
+    const max = lod.max(arr)
+    const min = lod.min(arr)
+    console.log(`Maximum : ${max} \nMinimum : ${min}`)
+}
+const nums = [1,2,3,4,5]
+findMinMax(nums)
+/*
+run:
+ =>npm update to update
+ =>npm uninstall to delete from node modules and package.json
+ on the terminal.
+ */
